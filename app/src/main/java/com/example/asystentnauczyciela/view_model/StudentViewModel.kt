9365abe84dd535a2a32179aefb63a3f8a00c01e2
RANDOM-V2N5q <44544007+RANDOM-V2N5q@users.noreply.github.com>
2020-11-30 18:17:17 +0100

    Add student view model

---
+++ app/src/main/java/com/example/asystentnauczyciela/view_model/StudentViewModel.kt
@@ -0,0 +1,34 @@
+package com.example.asystentnauczyciela.view_model
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.example.asystentnauczyciela.model.AppDatabase
+import com.example.asystentnauczyciela.model.Student
+import com.example.asystentnauczyciela.model.repositories.StudentRepository
+import kotlinx.coroutines.launch
+
+class StudentViewModel(application: Application): AndroidViewModel(application) {
+
+    val allStudent: LiveData<List<Student>>
+    private val studentRepository: StudentRepository
+
+    init {
+        val studentDao = AppDatabase.getDatabase(application).studentDao()
+        studentRepository = StudentRepository(studentDao)
+        allStudent = studentRepository.allStudents
+    }
+
+    fun addStudent(student: Student) {
+        viewModelScope.launch {
+            studentRepository.add(student)
+        }
+    }
+
+    fun deleteStudent(student: Student) {
+        viewModelScope.launch {
+            studentRepository.delete(student)
+        }
+    }
+}
