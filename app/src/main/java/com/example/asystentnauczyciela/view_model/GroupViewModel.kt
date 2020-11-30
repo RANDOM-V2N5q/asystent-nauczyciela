6906ddea883c9c9fd548cca42235f5c5c07acf3e
RANDOM-V2N5q <44544007+RANDOM-V2N5q@users.noreply.github.com>
2020-11-30 18:48:26 +0100

    Add group view model

---
+++ app/src/main/java/com/example/asystentnauczyciela/view_model/GroupViewModel.kt
@@ -0,0 +1,34 @@
+package com.example.asystentnauczyciela.view_model
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.example.asystentnauczyciela.model.AppDatabase
+import com.example.asystentnauczyciela.model.Group
+import com.example.asystentnauczyciela.model.repositories.GroupRepository
+import kotlinx.coroutines.launch
+
+class GroupViewModel(application: Application): AndroidViewModel(application) {
+
+    val allGroups: LiveData<List<Group>>
+    private val groupRepository: GroupRepository
+
+    init {
+        val groupDao = AppDatabase.getDatabase(application).groupDao()
+        groupRepository = GroupRepository(groupDao)
+        allGroups = groupDao.allGroups()
+    }
+
+    fun addGroup(group: Group) {
+        viewModelScope.launch {
+            groupRepository.add(group)
+        }
+    }
+
+    fun deleteGroup(group: Group) {
+        viewModelScope.launch {
+            groupRepository.delete(group)
+        }
+    }
+}
