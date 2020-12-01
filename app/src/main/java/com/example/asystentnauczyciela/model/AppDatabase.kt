f0717ba096711f3b2e29a4fdf7fc2b482558f720
RANDOM-V2N5q <44544007+RANDOM-V2N5q@users.noreply.github.com>
2020-12-01 15:50:23 +0100

    Add date type converter

--- app/src/main/java/com/example/asystentnauczyciela/model/AppDatabase.kt
+++ app/src/main/java/com/example/asystentnauczyciela/model/AppDatabase.kt
@@ -4,8 +4,10 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 
 @Database(entities = [Student::class, Group::class, Mark::class, StudentGroup::class], version = 1, exportSchema = false)
+@TypeConverters(Converters::class)
 abstract class AppDatabase: RoomDatabase() {
 
     abstract fun studentDao(): StudentDao
