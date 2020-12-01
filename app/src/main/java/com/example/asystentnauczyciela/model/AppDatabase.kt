e60ba6d8c794cea98baa93fe2e54706fa2bd70a7
RANDOM-V2N5q <44544007+RANDOM-V2N5q@users.noreply.github.com>
2020-12-01 13:31:15 +0100

    Add missing entities to database

--- app/src/main/java/com/example/asystentnauczyciela/model/AppDatabase.kt
+++ app/src/main/java/com/example/asystentnauczyciela/model/AppDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Student::class], version = 1, exportSchema = false)
+@Database(entities = [Student::class, Group::class, Mark::class, StudentGroup::class], version = 1, exportSchema = false)
 abstract class AppDatabase: RoomDatabase() {
 
     abstract fun studentDao(): StudentDao
